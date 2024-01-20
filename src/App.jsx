e6e8e73bcd8367e88acb9966abb2ace790f66c7e
Rafiul29 <rafiulislam4472@gmail.com>
2024-01-20 20:44:06 +0600

    project initialize

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <button className="bg-white  border border-gray-200 h-12  w-12 m-1  rounded-md leading-9 text-lg cursor-pointer">
+        X
+      </button>
+    </>
+  );
+}
+
+export default App;
