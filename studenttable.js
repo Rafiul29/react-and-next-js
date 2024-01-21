20c71b537d8221dc8a3d711c5f49626acc47e723
Rafiul29 <rafiulislam4472@gmail.com>
2024-01-22 02:07:09 +0600

    data added

---
+++ studenttable.js
@@ -0,0 +1,44 @@
+export const studentsTable = [
+  {
+    classId: 1,
+    className: "one",
+    students: [
+      {
+        id: 1,
+        name: "rafiul",
+        score: "A+",
+        percentage: 90,
+      },
+      {
+        id: 2,
+        name: "shohel",
+        score: "A+",
+        percentage: 98,
+      },
+      {
+        id: 3,
+        name: "Arif",
+        score: "A+",
+        percentage: 96,
+      },
+      {
+        id: 4,
+        name: "tanvir",
+        score: "A",
+        percentage: 89,
+      },
+      {
+        id: 5,
+        name: "badhon",
+        score: "A+",
+        percentage: 91,
+      },
+      {
+        id: 6,
+        name: "badhon",
+        score: "A+",
+        percentage: 91,
+      },
+    ],
+  },
+];
