862d57deba04534dec7d81af4e85d2db3f9d1722
Rafiul29 <rafiulislam4472@gmail.com>
2024-01-28 00:35:49 +0600

    code clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 function App() {
   return (
     <>
-      <button className="bg-white  border border-gray-200 h-12  w-12 m-1  rounded-md leading-9 text-lg cursor-pointer">
-        X
-      </button>
+     app
     </>
   );
 }
