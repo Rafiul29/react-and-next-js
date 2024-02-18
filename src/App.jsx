77f3b8c4750ad3c9807d25e98fbf824fd4df109a
Rafiul29 <rafiulislam4472@gmail.com>
2024-02-18 23:27:40 +0600

    task ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import TaskBoard from "./task/TaskBoard";
+
 function App() {
   return (
     <>
-     app
+      <Header/>
+      <HeroSection/>
+      <TaskBoard/>
+      <Footer/>
     </>
   );
 }
