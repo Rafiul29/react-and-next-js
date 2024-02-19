f7cca23cbf758c4427b24daf8d11941c16da9151
Rafiul29 <rafiulislam4472@gmail.com>
2024-02-19 23:10:56 +0600

    book finder app ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import BookList from "./book/BookList";
+
 function App() {
   return (
     <>
-     app
+      <Navbar />
+      <main className="my-10 lg:my-14">
+        <Header/>
+        <BookList/>
+      </main>
+      <Footer />
     </>
   );
 }
