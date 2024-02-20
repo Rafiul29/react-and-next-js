c9a19a933ef27fd83598a5bd96157c55f96f0715
Rafiul29 <rafiulislam4472@gmail.com>
2024-02-21 02:33:15 +0600

    sort and search implementation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import Navbar from "./Navbar";
+import { useState } from "react";
 import BookList from "./book/BookList";
+import { booksList } from "./booksList";
 
 function App() {
+  const [books, setBooks] = useState([...booksList]);
+
+  function handleSearch(searchTerm) {
+    const filterBooks = books.filter((book) =>
+      book.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setBooks([...filterBooks]);
+  }
+
+  function handleSort(sortBy) {
+    if (sortBy === "name_asc") {
+      // const sortName_Asc = books.sort((a, b) => (a.name > b.name ? 1 : -1));
+      const sortName_Asc = books.sort((a, b) => a.name > b.name);
+      setBooks([...sortName_Asc]);
+    } else if (sortBy === "name_desc") {
+      const sortName_Desc = books.sort((a, b) => a.name < b.name);
+      // const sortName_Desc = books.sort((a, b) => (a.name < b.name ? 1 : -1));
+
+      setBooks([...sortName_Desc]);
+    } else if (sortBy === "year_asc") {
+      const sortAscYear = books.sort((a, b) => a.year - b.year);
+      setBooks([...sortAscYear]);
+    } else if (sortBy === "year_desc") {
+      const sortDescYear = books.sort((a, b) => b.year - a.year);
+      setBooks([...sortDescYear]);
+    } else {
+      setBooks([...books]);
+    }
+  }
   return (
     <>
       <Navbar />
       <main className="my-10 lg:my-14">
-        <Header/>
-        <BookList/>
+        <Header onSearch={handleSearch} onSort={handleSort} />
+        <BookList books={books} />
       </main>
       <Footer />
     </>
---
+++ src/booksList.js
@@ -0,0 +1,46 @@
+export const booksList = [
+  {
+    id: 1,
+    avater:
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*zBRkcBbsjWzcPjtcxqhb3Q.jpeg",
+    name: "avaScript and Jquery 1",
+    author: "Jon Duckett",
+    year:1990,
+    price: 34,
+    rating: 4,
+    isFavourite: false,
+  },
+  {
+    id: 2,
+    avater:
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*zBRkcBbsjWzcPjtcxqhb3Q.jpeg",
+    name: "davaScript The Definitive 1",
+    author: " David Flanagan",
+    price: 70,
+    year:1991,
+    rating: 5,
+    isFavourite: false,
+  },
+  {
+    id: 3,
+    avater:
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*zBRkcBbsjWzcPjtcxqhb3Q.jpeg",
+    name: "bavaScript and Jquery 2",
+    author: "Jon Duckett",
+    year:1992,
+    price: 10,
+    rating: 2,
+    isFavourite: false,
+  },
+  {
+    id: 4,
+    avater:
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*zBRkcBbsjWzcPjtcxqhb3Q.jpeg",
+    name: "avaScript The Definitive 2",
+    author: " David Flanagan",
+    year:1993,
+    price: 12,
+    rating: 1,
+    isFavourite: false,
+  },
+];
