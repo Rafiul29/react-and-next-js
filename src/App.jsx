c3dd79702f0740a8edf96063818c07d51fa434b9
Rafiul29 <rafiulislam4472@gmail.com>
2024-05-28 15:52:23 +0600

    context api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard";
+
 function App() {
+// 8cf083632416591cc81ebe5272f384c7
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
   return (
-    <>
-     app
-    </>
+   <div className="grid place-items-center h-screen">
+   <Header/>
+   <main>
+    <section>
+      <WeatherBoard/>
+    </section>
+   </main>
+   </div>
+
   );
 }
 
---
+++ src/hooks/useWeather.js
@@ -0,0 +1,82 @@
+import { useEffect } from "react";
+import { useState } from "react"
+
+
+const useWeather = () => {
+  const [weatherData, setWeatherData] = useState({
+    location: "",
+    climate: "",
+    temperature: "",
+    minTemperature: "",
+    maxTemperature: "",
+    humidity: "",
+    cloudPercentage: "",
+    wind: "",
+    time: "",
+    longitude: "",
+    latitude: ""
+  });
+
+  const [loading, setLoading] = useState({
+    state: "false",
+    message: ""
+  })
+  const [error, setError] = useState(null)
+
+  const fetchWeatherData = async (latitude, longitude) => {
+    try {
+      setLoading({
+        ...loading,
+        state: true,
+        message: "Fetching weather data..."
+      })
+      // TODO: make the fetch call
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`)
+
+      if (!response.ok) {
+        const errorMessage = `Fetching weather data failed: ${response.status}`
+        throw new Error(errorMessage)
+      }
+      const data = await response.json()
+      const updateWeatherData = {
+        ...weatherData,
+        location: data?.name,
+        climate: data?.weather[0]?.main,
+        temperature: data?.main.temp,
+        minTemperature: data?.main?.temp_min,
+        maxTemperature: data?.main?.temp_max,
+        humidity: data?.main?.humidity,
+        cloudPercentage: data?.clouds?.all,
+        wind: data?.wind?.speed,
+        time: data?.dt,
+        longitude: longitude,
+        latitude: latitude
+      }
+
+      setWeatherData(updateWeatherData)
+    } catch (err) {
+      setError(err)
+    } finally {
+      setLoading({
+        ...loading,
+        state: false,
+        message: ""
+      })
+    }
+  }
+  useEffect(() => {
+    setLoading({
+      loading: true,
+      message: "Finding Loaction..."
+    })
+    navigator.geolocation.getCurrentPosition(function (position) {
+      fetchWeatherData(position.coords.latitude, position.coords.latitude)
+    })
+  }, [])
+
+  return {
+    weatherData, error, loading
+  }
+}
+
+export default useWeather;
